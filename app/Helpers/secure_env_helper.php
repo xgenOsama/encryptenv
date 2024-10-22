72f0c81507db42e7db1cc29dbceae0ec02c33fe4
osama mohamed <xgen.osama@gmail.com>
2024-10-23 02:06:26 +0300

    init

--- app/Helpers/secure_env_helper.php
+++ app/Helpers/secure_env_helper.php
@@ -1,7 +1,7 @@
 <?php
 // app/Helpers/env_helper.php
 
-function secureEnv(string $key, $defualt = null): ?string
+function secureEnv(string $key, $default = null): ?string
 {
     // Load the shared object
     $ffi = FFI::cdef(
@@ -11,7 +11,19 @@ function secureEnv(string $key, $defualt = null): ?string
 
     // Call the C function
     $result = $ffi->get_env_value($key);
+    // Convert the C string to a PHP string
+    $envValue = $result ? FFI::string($result) : $default;
+    // Check if the result is null
+    if ($result === null) {
+        return $default; // Return default if the result is null
+    }
+    // Check for pattern matches
+    if (preg_match('/^"\\${([A-Z_]+)}"$/', $envValue, $matches)) {
+        // The matched variable name
+        $variableName = $matches[1];
+        // Call secureEnv again if you want to retrieve the value of the matched variable
+        return secureEnv($variableName);
+    }
 
-    // Convert the C string to a PHP string and return it
-    return $result ? FFI::string($result) : $defualt;
+    return $envValue; // Return the original value if no pattern matched
 }
